a3b008d9a6e9eeee66088ff606397e268e9393c6
원도훈 <dnjsehgns98@gmail.com>
2024-10-23 11:59:04 +0900

    styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import "./App.scss";
-import { contents } from "./assets/data/data";
+import styled from 'styled-components';
+import {contents} from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
 function App() {
-  return (
-    <main>
-      <Header />
-      <section>
-        {contents.map((el) => (
-          <Content key={el.id} content={el} />
-        ))}
-      </section>
-    </main>
-  );
+    return (
+        <Main>
+            <Header/>
+            <Section>
+                {contents.map((el) => (
+                    <Content key={el.id} content={el}/>
+                ))}
+            </Section>
+        </Main>
+    );
 }
 
 export default App;
+
+const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Section = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 20px;
+    padding: 20px 40px;
+`;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
+import styled from 'styled-components';
+
 export default function Header() {
-  return (
-    <header className="header-container">
-      <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
-      </ul>
-    </header>
-  );
+    return (
+        <HeaderContainer>
+            <h1>OZ코딩스쿨</h1>
+            <MenuContainer>
+                <li>로그인</li>
+                <li>회원가입</li>
+                <li>내클래스</li>
+            </MenuContainer>
+        </HeaderContainer>
+    );
 }
+
+const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 20px;
+    padding: 20px;
+    background-color: black;
+`;
+
+const MenuContainer = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+
+    li {
+        font-size: 16px;
+        font-weight: 400;
+        list-style: none;
+        color: white;
+    }
+`;
